04effc0bb6c2751d40431dd8f53dd25ffe8b11f5
Tim <garrettrane@gmail.com>
2022-03-23 17:56:15 +0300

    second edition

--- OMP/main.js
+++ OMP/main.js
@@ -1,6 +1,17 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms =+prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('Сколько фильмов вы уже смотрели?','');
+    }
+    
+}
+
+start();
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,15 +21,43 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?'),
-      b = prompt('На сколько вы его оцените?', ''),
-      c = prompt('Один из последних просмотренных фильмов?'),
-      d = prompt('На сколько вы его оцените?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+function rememberMyFilms (){
+    for (let i = 0; i < 1; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?'),
+              b = prompt('На сколько вы его оцените?', '');   
+        if(a.length != null && b.length != null && a != '' && b != '' && a.length < 50){
+           personalMovieDB.movies[a] = b;
+           console.log('done');
+        } else {
+           alert('Обязательно заполните строку');
+           i--;
+        }
+   }
+}
+rememberMyFilms();
 
-console.log(personalMovieDB);
+function detectPersonalLevel (){
+    if (personalMovieDB.count < 10){
+        console.log('Просмотренно довольно мало фильмов');
+    }else if (personalMovieDB > 10 && personalMovieDB < 30){
+        console.log('Вы классический зритель');
+    }else {
+        console.log('Вы киноман');
+    }
+}
 
-     
+detectPersonalLevel();
+
+function writeYourGenre( hidding){
+    for (let i = 1; i <= 3; i++){
+        const question = prompt(`Ваш любимый жанр по номером ${i}`);
+        if(question != null){
+            hidding[i-1] = question; 
+        }
+    }
+}
+
+writeYourGenre(personalMovieDB.genres);
+
 
